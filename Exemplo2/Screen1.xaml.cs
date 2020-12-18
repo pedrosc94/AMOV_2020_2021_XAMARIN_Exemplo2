e850e9767c110864f6705e6025bf3deff5cae1bc
Pedro Carvalho <pedrosilvacar@gmail.com>
2020-12-18 14:25:40 +0000

    screen3 stuff

--- Exemplo2/Screen1.xaml.cs
+++ Exemplo2/Screen1.xaml.cs
@@ -12,22 +12,18 @@ namespace Exemplo2
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Screen1 : ContentPage
     {
-
-        public class ListaClassNumeros
-        {
-            public int Numero { get; set; }
-        }
+        public class ClassListaNumeros { public int Numero { get; set; } }
 
         public Screen1()
         {
             InitializeComponent();
 
             //cria nova lista
-            var ListaNumeros = new List<ListaClassNumeros>();
+            var ListaNumeros = new List<ClassListaNumeros>();
             //adiciona um novo elemento a list
             for (int i = 1; i<=1000; i++)
             {
-                ListaNumeros.Add(new ListaClassNumeros { Numero = i });
+                ListaNumeros.Add(new ClassListaNumeros { Numero = i });
             }
             //associa a lista criada ao ListView "listaScreen1" no XAML
             listaScreen1.ItemsSource = ListaNumeros;
--- Exemplo2/Screen3.xaml.cs
+++ Exemplo2/Screen3.xaml.cs
@@ -13,23 +13,24 @@ namespace Exemplo2
     public partial class Screen3 : ContentPage
     {
         int quantidade = 15;
-        public class ListaClassImagemNomeNumero
+        public class ClassListaImagemNomeNumero
         {
             public Image Imagem { get; set; }
             public String Nome { get; set; }
             public int Numero{ get; set; }
         }
+
         public Screen3()
         {
             InitializeComponent();
 
-            var ListaImagemNomeNumero = new List<ListaClassImagemNomeNumero>();
+            var ListaImagemNomeNumero = new List<ClassListaImagemNomeNumero>();
             for (int i = 0; i <= quantidade; i++)
             {
-                ListaImagemNomeNumero.Add(new ListaClassImagemNomeNumero
+                ListaImagemNomeNumero.Add(new ClassListaImagemNomeNumero
                 {
-                    Imagem = "something_here.jpg",//verificar isto
-                    Nome = "test",
+                    Imagem = {""},//verificar isto
+                    Nome = "Com dollar ${i}" + " Sem dollar {i}",
                     Numero = i
                 });
             }
